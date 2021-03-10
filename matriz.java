67f7b068c32ed5fac5a30a277e6a1f6c1c9615e5
Martin-02 <martinberlanas2002@gmail.com>
2021-03-10 23:09:42 +0100

    Creacion matriz

---
+++ matriz.java
@@ -0,0 +1,17 @@
+public class matriz {
+
+    public static void main(String args[]) {
+
+        int filas, columnas, mundoMuere
+
+        funcion New(int F, int C, int MM)
+
+            filas = f;
+            columnas = C
+            Si (MM < 0) luego MM=0)
+            MundoMuere = MM
+
+        dimensionarMapa(filas, columnas);
+        fin funcion
+    }
+}
