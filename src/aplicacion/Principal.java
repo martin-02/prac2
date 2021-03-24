b4bbb1fe4409c9ab64d5a34e837c27996565dd4b
Martin <martin.berlanasmolera@usp.ceu.es>
2021-03-24 13:08:21 +0100

    Ordeno clase Principal.java

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -5,6 +5,7 @@ import java.lang.InterruptedException;
 public class Principal {
     public static void main(String[] args) {
         try {
+            
             Tablero tablero = new Tablero();
             System.out.println("SIMULACIÓN CON TABLERO LEÍDO");
             tablero.leerEstadoActual();
@@ -22,6 +23,7 @@ public class Principal {
             System.out.println(tablero);
 
             for(int i = 0; i <= 15; i++) {
+
                 TimeUnit.SECONDS.sleep(1);
                 tablero.transitarAlEstadoSiguiente();
                 System.out.println(tablero);
